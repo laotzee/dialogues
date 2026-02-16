1377c506239a3d506e383ea0e4e8c8df8131d5fa
laotzee <laotzeabreu@proton.me>
2026-02-16 11:17:56 -0400

    Save_posts command now process many files at once from stdin

--- app/cli/cli.py
+++ app/cli/cli.py
@@ -1,15 +1,30 @@
 import click
+import sys
 from .helpers import create_post
 from app import db
+from app.models.models import Post
 
 
 def cli_commands(app):
     """Register commands for flask app"""
 
-    @app.cli.command("save_post")
+    @app.cli.command("save_posts")
     @click.argument("file")
-    def save_post(file):
-        """Loads post to the database"""
-        new_post = create_post(file)
-        db.session.add(new_post)
+    def save_posts(files):
+        """Loads posts form stdin to the database"""
+        posts = sys.stdin
+        for file in files:
+            file = file.strip()
+            new_post = create_post(file)
+            db.session.add(new_post)
+        db.session.commit()
+
+
+    @app.cli.command("publish_post")
+    def publish_post():
+        """Changes to public the oldest unpublished posts on the database"""
+        stmt = db.select(Post).order_by(Post.created).where(
+                Post.is_published == False)
+        post = db.session.scalars(stmt).first()
+        post.is_published = True
         db.session.commit()
