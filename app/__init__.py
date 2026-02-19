367e517dd68684ca9c14a8bd6aa8a792b23f086d
laotzee <laotzeabreu@proton.me>
2026-02-19 17:29:04 -0400

    Create babel_setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, g, url_for
-from .extensions import db, migrate, babel, get_locale
+from .extensions import db, migrate, babel, babel_setup, get_locale
 from .routes.main import blueprint
 from .cli.cli import cli_commands
 from config import app_env
@@ -14,16 +14,6 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db, render_as_batch=True)
     babel.init_app(app, locale_selector=get_locale)
-
-    @app.before_request
-    def before_request():
-        g.locale = get_locale()
-
-    @app.context_processor
-    def inject_url_helpers():
-        def local_url(endpoint, **values):
-            return url_for(f'blueprint_{g.locale}.{endpoint}', **values)
-    
-        return dict(local_url=local_url)
+    babel_setup(app)
 
     return app
--- app/extensions.py
+++ app/extensions.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import MetaData
 from flask_migrate import Migrate
 
-from flask import Flask, request
+from flask import Flask, request, g, url_for
 from flask_babel import Babel
 
 convention = {
@@ -25,3 +25,16 @@ def get_locale():
     if request.path.startswith('/es'):
         return 'es'
     return 'en'
+
+def babel_setup(app):
+    """Defines babel functions in app context"""
+    @app.before_request
+    def before_request():
+        g.locale = get_locale()
+
+    @app.context_processor
+    def inject_url_helpers():
+        def local_url(endpoint, **values):
+            return url_for(f'blueprint_{g.locale}.{endpoint}', **values)
+    
+        return dict(local_url=local_url)
